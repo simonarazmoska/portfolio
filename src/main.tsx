789db585a66aaa3f1b74e90614f9f5b8491bcff1
Simona Razmoska <simona.razmoska@hotmail.com>
2024-07-24 20:40:20 +1000

    feat: add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { theme } from "./theme";
 
 import "@fontsource/inter/300.css";
 import "@fontsource/inter/400.css";
@@ -9,5 +12,8 @@ import "@fontsource/inter/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   </React.StrictMode>
 );
