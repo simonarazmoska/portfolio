eafda2058e0930321372c9993eb4ca69ea9256aa
Simona Razmoska <simona.razmoska@hotmail.com>
2024-07-26 22:58:54 +1000

    style: add custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ export default {
     },
     extend: {
       colors: {
+        headerText: "#EAD8FC",
         headerBackground: "#151515"
+      },
+      fontSize: {
+        "4.5xl": "2.5rem",
+        s20lh22: [
+          "20px",
+          {
+            lineHeight: "22px"
+          }
+        ]
       }
     }
   },
