36b0eab9001bb7648b07e2eea5a903cf8a16a0a9
Simona Razmoska <simona.razmoska@hotmail.com>
2024-08-18 20:46:51 +1000

    build: ignore move files when building production

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    {
+      name: "exclude-file-type",
+      transform(code, id) {
+        if (id.endsWith(".mov")) {
+          return null;
+        }
+        return code;
+      }
+    }
+  ]
 });
