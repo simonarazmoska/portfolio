c64e860e2c2e2c31fa4871e52c801fb5f15526d7
Simona Razmoska <simona.razmoska@hotmail.com>
2025-07-24 18:15:24 +1000

    feat: update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,62 +1,51 @@
-import { Box, Link } from "@mui/material";
+import { Icon } from "@iconify/react";
+import { Box, Link, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
-    <Box className="tw-text-textMain tw-px-12 tw-py-8 tw-space-x-8 tw-bg-secondary tw-border-t-[0px]">
-      <Box className="tw-flex tw-flex-row tw-justify-between">
-        <Box className="tw-flex tw-flex-col tw-justify-end">
-          <p className="tw-text-textMain">&copy; 2025 Simona Razmoska</p>
-        </Box>
-        <Box className="tw-flex tw-flex-row tw-space-x-6">
-          <Box className="tw-max-w-[175.57px] tw-space-y-4">
-            <Box className="tw-flex tw-flex-row tw-items-center tw-px-6">
-              <p className="tw-font-[600] tw-text-textMain tw-text-[16px]">CONTACT</p>
-            </Box>
-            <Link
-              underline="hover"
-              href="mailto:simona.razmoska@hotmail.com"
-              className="tw-flex tw-flex-row tw-items-center tw-px-6"
-              sx={{
-                textDecorationColor: "textMain",
-                "&:hover": {
-                  textDecorationColor: "black"
-                }
-              }}
-            >
-              <p className="tw-text-textMain tw-text-xl">Email</p>
-            </Link>
-            <Link
-              underline="hover"
-              href="https://au.linkedin.com/in/simona-razmoska"
-              className="tw-flex tw-flex-row tw-items-center tw-px-6"
-              rel="noopener noreferrer"
-              target="_blank"
-              sx={{
-                textDecorationColor: "textMain",
-                "&:hover": {
-                  textDecorationColor: "black"
-                }
-              }}
-            >
-              <p className="tw-text-textMain tw-text-xl">LinkedIn</p>
-            </Link>
-            <Link
-              underline="hover"
-              href="https://medium.com/@simonarazmoska"
-              className="tw-flex tw-flex-row tw-items-center tw-px-6"
-              rel="noopener noreferrer"
-              target="_blank"
-              sx={{
-                textDecorationColor: "textMain",
-                "&:hover": {
-                  textDecorationColor: "black"
-                }
-              }}
-            >
-              <p className="tw-text-textMain tw-text-xl">Medium</p>
-            </Link>
+    <Box className="tw-h-[460px] tw-bg-textMain tw-flex tw-flex-col tw-px-24 tw-py-20 tw-gap-[90px] tw-justify-center tw-items-start">
+      <Box className="tw-flex tw-flex-col tw-gap-12">
+        <Typography variant="h1" className="tw-text-secondary">
+          Contact
+        </Typography>
+      </Box>
+      <Box className="tw-flex tw-flex-col tw-gap-[90px] tw-items-start tw-pt-12 tw-pb-6 tw-border-t-[1px] tw-border-primary tw-w-full">
+        <Box className="tw-flex tw-flex-row tw-justify-between tw-items-end tw-w-full">
+          <FooterColumn mainText="GET IN TOUCH" linkText="Email" linkUrl="mailto:simona.razmoska@hotmail.com" />
+          <FooterColumn mainText="FIND ME ON" linkText="LinkedIn" linkUrl="https://www.linkedin.com/in/simona-razmoska/" />
+          <FooterColumn mainText="READ MORE" linkText="Medium" linkUrl="https://medium.com/@simonarazmoska" />
+          <Box>
+            <Typography variant="body" className="tw-text-primary">
+              &copy; 2025 Simona Razmoska
+            </Typography>
           </Box>
         </Box>
+      </Box>
+    </Box>
+  );
+};
+
+interface FooterColumnProps {
+  mainText: string;
+  linkText: string;
+  linkUrl: string;
+}
+
+const FooterColumn = ({ mainText, linkText, linkUrl }: FooterColumnProps) => {
+  return (
+    <Box className="tw-w-[224px] tw-flex tw-flex-col tw-gap-4 tw-items-start">
+      <Box>
+        <Typography variant="title" className="tw-text-primary">
+          {mainText}
+        </Typography>
+      </Box>
+      <Box className="tw-flex tw-flex-col tw-gap-2">
+        <Link href={linkUrl} underline="none" className="tw-cursor-pointer tw-gap-2 tw-py-2 tw-flex tw-items-center">
+          <Typography variant="title" className="tw-text-primary">
+            {linkText}
+          </Typography>
+          <Icon icon="material-symbols:arrow-forward" className="tw-text-primary" />
+        </Link>
       </Box>
     </Box>
   );
